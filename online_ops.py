4ba87ef309cc884a4799581c8ffd7569e27fa8ee
toto <riedotoine@gmail.com>
2021-12-16 21:42:22 +0100

    ajout de fonction dans online_ops.py

--- online_ops.py
+++ online_ops.py
@@ -3,3 +3,31 @@ import wikipedia
 import pywhatkit as kit
 from decouple import config
 
+OPENWEATHER_APP_ID = config("1d100c745149ebfeb3ff7adc02eedf54")
+
+
+def find_my_ip():
+    ip_address = requests.get('https://api64.ipify.org/?format=json')
+    return ip_address["ip"]
+
+def search_on_wikipedia(query):
+    results = wikipedia.summary(query, sentences = 2)
+    return results
+
+def search_on_google(query):
+    kit.search(query)
+
+def get_weather_report(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
+
+def get_random_joke():
+    headers = {
+        'Accept': 'application/json'
+    }
+    res = requests.get("https://icanhazdadjoke.com/", header)
+    return res["joke"]
